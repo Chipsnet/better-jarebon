e4ed222de1f987129ef480937494a59a4bf100c9
Chipsnet <me@mina.to>
2025-10-16 22:24:48 +0900

    部屋作成ページ

--- apps/frontend/app/routes/_index.tsx
+++ apps/frontend/app/routes/_index.tsx
@@ -1,12 +1,16 @@
 import { Box, Button, Center, Heading, HStack } from "@chakra-ui/react";
+import { Link } from "react-router";
+
 
 export default function TopPage() {
   return <Center h="100vh">
     <Box>
       <Center><Heading size={"3xl"}>Better Jarebon</Heading></Center>
       <HStack >
-              <Button>部屋を作る</Button>
-      <Button>部屋に入る</Button>
+        <Button asChild>
+          <Link to="/create-room">部屋を作る</Link>
+        </Button>
+        <Button>部屋に入る</Button>
       </HStack >
     </Box>
   </Center>;
--- apps/frontend/app/routes/create-room.tsx
+++ apps/frontend/app/routes/create-room.tsx
@@ -1,3 +1,63 @@
+import { Button, Container, RadioCard, NumberInput, Field, HStack } from "@chakra-ui/react";
+
 export default function CreateRoomPage() {
-    return <div>Create Room Page</div>;
-    }
+    return <Container>
+        <div>部屋作成ページ</div>
+        <Field.Root>
+            <Field.Label>ターン数</Field.Label>
+            <NumberInput.Root size={"lg"} defaultValue="6" min={0}>
+                <NumberInput.Control />
+                <NumberInput.Input />
+            </NumberInput.Root>
+        </Field.Root>
+        <Field.Root>
+            <Field.Label>文字数</Field.Label>
+            <NumberInput.Root size={"lg"} defaultValue="120" min={0}>
+                <NumberInput.Control />
+                <NumberInput.Input />
+            </NumberInput.Root>
+        </Field.Root>
+        <RadioCard.Root>
+            <RadioCard.Label>時間制限</RadioCard.Label>
+            <HStack>
+                <RadioCard.Item value="aaaa">
+                    <RadioCard.ItemHiddenInput />
+                    <RadioCard.ItemControl>
+                        <RadioCard.ItemContent>
+                            <RadioCard.ItemText>無効</RadioCard.ItemText>
+                            <RadioCard.ItemDescription>
+                                時間制限はなく、タイマーも表示されません。
+                            </RadioCard.ItemDescription>
+                        </RadioCard.ItemContent>
+                        <RadioCard.ItemIndicator />
+                    </RadioCard.ItemControl>
+                </RadioCard.Item>
+                <RadioCard.Item value="bbbb">
+                    <RadioCard.ItemHiddenInput />
+                    <RadioCard.ItemControl>
+                        <RadioCard.ItemContent>
+                            <RadioCard.ItemText>表示</RadioCard.ItemText>
+                            <RadioCard.ItemDescription>
+                                タイマーを表示しますが、超過しても何も起こりません。
+                            </RadioCard.ItemDescription>
+                        </RadioCard.ItemContent>
+                        <RadioCard.ItemIndicator />
+                    </RadioCard.ItemControl>
+                </RadioCard.Item>
+                <RadioCard.Item value="bbbb">
+                    <RadioCard.ItemHiddenInput />
+                    <RadioCard.ItemControl>
+                        <RadioCard.ItemContent>
+                            <RadioCard.ItemText>有効</RadioCard.ItemText>
+                            <RadioCard.ItemDescription>
+                                タイマーを表示します。超過すると強制的に次のページへ進みます。
+                            </RadioCard.ItemDescription>
+                        </RadioCard.ItemContent>
+                        <RadioCard.ItemIndicator />
+                    </RadioCard.ItemControl>
+                </RadioCard.Item>
+            </HStack>
+        </RadioCard.Root>
+        <Button>部屋を作る</Button>
+    </Container>;
+}
